320f586139f59ed1718bbcaa976cc2d4b01ed73d
droppledev <ayasfn@gmail.com>
2018-12-02 17:22:30 +0700

    rev2 member and employee done

--- kpintar/resources/views/employee/employee-add.blade.php
+++ kpintar/resources/views/employee/employee-add.blade.php
@@ -1,46 +1,44 @@
 @extends('layout')
 @section('realpage')
 <div id="page-wrapper">
-    <div id="page-wrapper">
-        <h3>Tambah Karyawan</h3>
-        <form action="{{ Route('admin.employee')  }}">
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" class="form-control" placeholder="Username">
-            </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" class="form-control" placeholder="Password">
-            </div>
-            <div class="form-group">
-                <label>Re-Enter Password</label>
-                <input type="password" class="form-control" placeholder=" Re-Enter Password">
-            </div>
-            <div class="form-group">
-                <label>Status</label>
-                <select class="form-control">
+    <h3>Tambah Karyawan</h3>
+    <form action="{{ Route('admin.employee')  }}">
+        <div class="form-group">
+            <label>Username</label>
+            <input type="text" class="form-control" placeholder="Username">
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" class="form-control" placeholder="Password">
+        </div>
+        <div class="form-group">
+            <label>Re-Enter Password</label>
+            <input type="password" class="form-control" placeholder=" Re-Enter Password">
+        </div>
+        <div class="form-group">
+            <label>Status</label>
+            <select class="form-control">
                     <option>Aktif</option>
                     <option>Tidak Aktif</option>
                     <option>Cuti</option>
                 </select>
-            </div>
-            <div class="form-group">
-                <label>Alamat</label>
-                <input type="text" class="form-control" placeholder="Alamat">
-            </div>
-            <div class="form-group">
-                <label>No. Telp</label>
-                <input type="text" class="form-control" placeholder="No. Telp">
-            </div>
-            <div class="form-group">
-                <label>IsManajer</label>
-                <select class="form-control">
+        </div>
+        <div class="form-group">
+            <label>Alamat</label>
+            <input type="text" class="form-control" placeholder="Alamat">
+        </div>
+        <div class="form-group">
+            <label>No. Telp</label>
+            <input type="text" class="form-control" placeholder="No. Telp">
+        </div>
+        <div class="form-group">
+            <label>IsManajer</label>
+            <select class="form-control">
                     <option>Tidak</option>
                     <option>Ya</option>
                 </select>
-            </div>
-            <button type="submit" class="btn btn-success">Submit</button>
-        </form>
-    </div>
+        </div>
+        <button type="submit" class="btn btn-success">Submit</button>
+    </form>
 </div>
 @endsection
--- kpintar/resources/views/member/member-add.blade.php
+++ kpintar/resources/views/member/member-add.blade.php
@@ -1,27 +1,25 @@
 @extends('layout')
 @section('realpage')
 <div id="page-wrapper">
-    <div id="page-wrapper">
-        <h3>Tambah Member</h3>
-            <form action="{{ Route('admin.member')  }}">
-                <div class="form-group">
-                    <label>Nama</label>
-                    <input type="text" class="form-control" placeholder="Nama" required>
-                </div>
-                <div class="form-group">
-                    <label>Alamat</label>
-                    <input type="text" class="form-control" placeholder="Alamat" required>
-                </div>
-                <div class="form-group">
-                    <label>No. Telp</label>
-                    <input type="text" class="form-control" placeholder="No. Telp" required>
-                </div>
-                <div class="form-group">
-                    <label>Tanggal Lahir</label>
-                    <input type="text" class="form-control" placeholder="dd/mm/yyyy" required>
-                </div>
-                <button type="submit" class="btn btn-success">Submit</button>
-            </form>
-    </div>
+    <h3>Tambah Member</h3>
+    <form action="{{ Route('admin.member')  }}">
+        <div class="form-group">
+            <label>Nama</label>
+            <input type="text" class="form-control" placeholder="Nama" required>
+        </div>
+        <div class="form-group">
+            <label>Alamat</label>
+            <input type="text" class="form-control" placeholder="Alamat" required>
+        </div>
+        <div class="form-group">
+            <label>No. Telp</label>
+            <input type="text" class="form-control" placeholder="No. Telp" required>
+        </div>
+        <div class="form-group">
+            <label>Tanggal Lahir</label>
+            <input type="text" class="form-control" placeholder="dd/mm/yyyy" required>
+        </div>
+        <button type="submit" class="btn btn-success">Submit</button>
+    </form>
 </div>
 @endsection
